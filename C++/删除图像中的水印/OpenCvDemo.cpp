2c42b44986dbece861f0f98f10996e880c946b39
GaoRenbao <1101923161@qq.com>
2023-11-27 16:04:36 +0800

    补充demo

---
+++ C++/删除图像中的水印/OpenCvDemo.cpp
@@ -0,0 +1,75 @@
+﻿/*
+OpenCv版本 opencv-4.5.5-vc14_vc15
+内容：删除图像中的水印
+博客：http://www.bilibili996.com/Course?id=b608bdd358e041478931886b8b08058c
+作者：高仁宝
+时间：2023.11
+
+来源：https://stackoverflow.com/questions/32125281/removing-watermark-out-of-an-image-using-opencv
+*/
+
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+    Mat im = imread("../images/YZeOg.jpg");
+
+    // 原图有点大。。。
+    //resize(im, im, Size(im.cols * 0.5, im.rows * 0.5), (0, 0), (0, 0), 3);
+    imshow("im", im);
+
+    Mat gr, bg, bw, dark;
+    cvtColor(im, gr, COLOR_BGR2GRAY);
+
+    // approximate the background
+    bg = gr.clone();
+    for (int r = 1; r < 5; r++)
+    {
+        Mat kernel2 = getStructuringElement(MORPH_ELLIPSE, Size(2 * r + 1, 2 * r + 1));
+        morphologyEx(bg, bg, MORPH_CLOSE, kernel2);
+        morphologyEx(bg, bg, MORPH_OPEN, kernel2);
+    }
+
+    // difference = background - initial
+    Mat dif = bg - gr;
+    // threshold the difference image so we get dark letters
+    threshold(dif, bw, 0, 255, THRESH_BINARY_INV | THRESH_OTSU);
+    // threshold the background image so we get dark region
+    threshold(bg, dark, 0, 255, THRESH_BINARY_INV | THRESH_OTSU);
+
+    // extract pixels in the dark region
+    vector<unsigned char> darkpix(countNonZero(dark));
+    int index = 0;
+    for (int r = 0; r < dark.rows; r++)
+    {
+        for (int c = 0; c < dark.cols; c++)
+        {
+            if (dark.at<unsigned char>(r, c))
+            {
+                darkpix[index++] = gr.at<unsigned char>(r, c);
+            }
+        }
+    }
+    // threshold the dark region so we get the darker pixels inside it
+    threshold(darkpix, darkpix, 0, 255, THRESH_BINARY | THRESH_OTSU);
+
+    // paste the extracted darker pixels
+    index = 0;
+    for (int r = 0; r < dark.rows; r++)
+    {
+        for (int c = 0; c < dark.cols; c++)
+        {
+            if (dark.at<unsigned char>(r, c))
+            {
+                bw.at<unsigned char>(r, c) = darkpix[index++];
+            }
+        }
+    }
+
+    imshow("BW", bw);
+    waitKey(0);
+}
---
+++ Python/使用Tessract-OCR识别验证码/main.py
@@ -0,0 +1,44 @@
+# OpenCv版本 OpenCvSharp4.6.0.66
+# 内容：使用Tessract-OCR识别验证码
+# 博客：
+# 作者：高仁宝
+# 时间：2023.11
+
+# tesserocr 无法安装。。。。
+# pip install tesserocr -i https://mirror.baidu.com/pypi/simple
+
+from io import BytesIO
+from PIL import Image
+import requests
+from tesserocr import PyTessBaseAPI
+import tesserocr
+import numpy as np
+import cv2
+
+user_agent = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36'
+headers = {'User-Agent': user_agent}
+
+url = 'http://www.bjsuperpass.com/captcha.svl?d=1503144107405'
+rs = requests.get(url, headers=headers, timeout=10)
+print('获取公交一卡通网站的验证码', rs.status_code)
+# TODO 获取cookies
+
+
+print('用BytesIO导入到Image，Numpy，Opencv')
+s1 = BytesIO(rs.content)  # img = Image.open(BytesIO(resp.read()))
+#
+img = Image.open(s1)
+img = img.convert("RGB")
+im = np.array(img)
+cv2.imshow('src', im)
+cv2.waitKey(0)
+# cv2.imwrite('captcha.jpg', im)
+
+ocr = PyTessBaseAPI()
+# ocr.Init(".", "eng", tesseract.OEM_DEFAULT)
+ocr.SetVariable("tessedit_char_whitelist", "0123456789abcdefghijklmnopqrstuvwxyz")
+# ocr.SetPageSegMode(tesseract.PSM_AUTO)
+# ocr.SetImage(img)
+
+print('验证码是', tesserocr.image_to_text(img))
+# TODO 发送cookies
