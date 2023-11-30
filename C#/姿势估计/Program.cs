0b3138a0a5173b3e114ec7510ebe1e9ae5b4e7ee
GaoRenbao <1101923161@qq.com>
2023-11-30 16:39:31 +0800

    补充demo

--- C#/姿势估计/Program.cs
+++ C#/姿势估计/Program.cs
@@ -4,168 +4,140 @@ OpenCv版本 OpenCvSharp4.4.8.0.20230708
 博客：http://www.bilibili996.com/Course?id=f2a6235f09a849a9a93974ce8b00272f
 作者：高仁宝
 时间：2023.11
+
+参考代码：https://github.com/shimat/opencvsharp/tree/main/test/OpenCvSharp.Tests/calib3d/Calib3dTest.cs
 */
 
-//【没搞定。。。】
+using OpenCvSharp;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
-//using OpenCvSharp;
-//using System.Collections.Generic;
-//using System.IO;
-//using System.Linq;
+namespace demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> paths = new List<string>()
+            {
+                "../../../images/lefts/",
+                "../../../images/rights/"
+            };
+            List<string> imagesList = new List<string>();
+            foreach (string path in paths)
+            {
+                DirectoryInfo di = new DirectoryInfo(path);
+                FileInfo[] fis = di.GetFiles("*.jpg");
+                Mat srcImage = new Mat();
+                foreach (FileInfo fi in fis)
+                {
+                    imagesList.Add(fi.FullName);
+                }
+            }
 
-//namespace demo
-//{
-//    internal class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//            List<string> paths = new List<string>()
-//            {
-//                "../../../images/lefts/",
-//                "../../../images/rights/"
-//            };
-//            List<string> imagesList = new List<string>();
-//            foreach (string path in paths)
-//            {
-//                DirectoryInfo di = new DirectoryInfo(path);
-//                FileInfo[] fis = di.GetFiles("*.jpg");
-//                Mat srcImage = new Mat();
-//                foreach (FileInfo fi in fis)
-//                {
-//                    imagesList.Add(fi.FullName);
-//                }
-//            }
+            TermCriteria criteria = new TermCriteria(CriteriaTypes.Eps | CriteriaTypes.MaxIter, 30, 0.001);
 
-//            TermCriteria criteria = new TermCriteria(CriteriaTypes.Eps | CriteriaTypes.MaxIter, 30, 0.001);
+            // 棋盘格内角点数量（行数和列数）
+            Size BoardSize = new Size(9, 6);
 
-//            Size BoardSize = new Size(9, 6);
-//            int SquareSize = 25; //每格的宽度应设置为实际的毫米数
+            // 棋盘格尺寸（单位：米）
+            float squareSize = 0.025f;
 
-//            Mat img = new Mat();
-//            Mat gray = new Mat();
+            Mat img = new Mat();
+            Mat gray = new Mat();
 
-//            List<Mat> imagesPointsM = new List<Mat>();
-//            //List<List<Point3f>> objpoints = new List<List<Point3f>>();
-//            List<List<Point2f>> imgpoints = new List<List<Point2f>>();
+            List<List<Point3f>> objpoints = new List<List<Point3f>>();
+            List<List<Point2f>> imgpoints = new List<List<Point2f>>();
 
-//            for (int i = 0; i < imagesList.Count; i++)
-//            {
-//                img = new Mat(imagesList[i]);
-//                if (!img.Empty())
-//                {
-//                    Cv2.CvtColor(img, gray, ColorConversionCodes.BGR2GRAY);
+            for (int i = 0; i < imagesList.Count; i++)
+            {
+                img = new Mat(imagesList[i]);
+                if (!img.Empty())
+                {
+                    Cv2.CvtColor(img, gray, ColorConversionCodes.BGR2GRAY);
 
-//                    Point2f[] pointBuf;
-//                    bool found = Cv2.FindChessboardCorners(img, BoardSize, out pointBuf);
-//                    if (found == true)
-//                    {
-//                        Mat viewGray = new Mat();
-//                        Cv2.CvtColor(img, viewGray, ColorConversionCodes.BGR2GRAY);
-//                        Cv2.CornerSubPix(viewGray, pointBuf, new Size(11, 11), new Size(-1, -1), criteria);
-//                        imgpoints.Add(pointBuf.ToList());
-//                        imagesPointsM.Add(Mat.FromArray<Point2f>(pointBuf));
-//                        //objpoints.Add(GetChessboardObjectPoints(BoardSize, ((float)SquareSize / 1000.0f)));
+                    var corners = new Mat<Point2f>();
+                    bool found = Cv2.FindChessboardCorners(gray, BoardSize, corners);
+                    if (found == true)
+                    {
+                        var objectPointsArray = Create3DChessboardCorners(BoardSize, squareSize).ToArray();
+                        var imagePointsArray = corners.ToArray();
 
-//                        Cv2.DrawChessboardCorners(img, BoardSize, pointBuf, found);
-//                        Cv2.ImShow("img", img);
-//                        Cv2.WaitKey(10);
-//                    }
-//                }
-//            }
+                        //var objectPoints = Mat<Point3f>.FromArray(objectPointsArray);
+                        //var imagePoints = Mat<Point2f>.FromArray(imagePointsArray);
 
-//            // 执行相机标定
-//            Mat[] rvecs = new Mat[] { };
-//            Mat[] tvecs = new Mat[] { };
-//            Mat mtx = new Mat();
-//            Mat dist = new Mat();
-//            Mat[] objpoints = calcBoardCornerPositions(BoardSize, SquareSize, imagesList.Count);
-//            Cv2.CalibrateCamera(objpoints, imagesPointsM.ToArray(), new Size(gray.Cols, gray.Rows), mtx, dist, out rvecs, out tvecs, CalibrationFlags.None);
+                        objpoints.Add(objectPointsArray.ToList());
+                        imgpoints.Add(imagePointsArray.ToList());
+                    }
+                }
+            }
 
-//            //Vec3d[] rvecs = new Vec3d[] { };
-//            //Vec3d[] tvecs = new Vec3d[] { };
-//            //double[,] mtx = new double[,] { };
-//            //double[] dist = new double[] { };
-//            //Cv2.CalibrateCamera(objpoints, imgpoints, new Size(gray.Cols, gray.Rows), mtx, dist, out rvecs, out tvecs, CalibrationFlags.None);
+            var mtx = new double[,] { { 1, 0, 0 }, { 0, 1, 0 }, { 0, 0, 1 } };
+            var dist = new double[5];
+            Cv2.CalibrateCamera(objpoints, imgpoints, gray.Size(), mtx,
+                dist, out var rvecs, out var tvecs, CalibrationFlags.None);
 
-//            Mat objq = objpoints[0];//new Mat(9 * 6, 3, MatType.CV_64FC3);
+            List<Point3f> objPts = new List<Point3f>();
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    objPts.Add(new Point3f(j, i, 0));
+                }
+            }
 
-//            Mat axis = new Mat(3, 3, MatType.CV_32F);
-//            axis.At<int>(0, 0) = 3;
-//            axis.At<int>(1, 1) = 3;
-//            axis.At<int>(2, 2) = -3;
+            var axis = new[]
+            {
+                new Point3f(3,0,0),
+                new Point3f(0,3,0),
+                new Point3f(0,0,-3),
+            };
 
-//            Mat rvecs2 = new Mat() { };
-//            Mat tvecs2 = new Mat() { };
-//            Mat imgpts = new Mat() { };
-//            Mat jac = new Mat() { };
+            foreach (string fname in imagesList)
+            {
+                img = Cv2.ImRead(fname);
+                Cv2.CvtColor(img, gray, ColorConversionCodes.BGR2GRAY);
+                Point2f[] corners;
+                bool found = Cv2.FindChessboardCorners(img, BoardSize, out corners);
+                if (found == true)
+                {
+                    var corners2 = Cv2.CornerSubPix(gray, corners, new Size(11, 11), new Size(-1, -1), criteria);
 
-//            foreach (string fname in imagesList)
-//            {
-//                img = Cv2.ImRead(fname);
-//                Cv2.CvtColor(img, gray, ColorConversionCodes.BGR2GRAY);
-//                Point2f[] corners;
-//                bool found = Cv2.FindChessboardCorners(img, BoardSize, out corners);
-//                if (found == true)
-//                {
-//                    var corners2 = Cv2.CornerSubPix(gray, corners, new Size(11, 11), new Size(-1, -1), criteria);
+                    var rvec = new double[] { };
+                    var tvec = new double[] { };
+                    Cv2.SolvePnP(objPts, corners2, mtx, dist, ref rvec, ref tvec);
 
-//                    // Find the rotation and translation vectors.
-//                    Cv2.SolvePnP(objq, Mat.FromArray<Point2f>(corners2), mtx, dist, rvecs2, tvecs2);
+                    Cv2.ProjectPoints(axis, rvec, tvec, mtx, dist, out var imgPts, out var jacobian);
+                    img = draw(img, corners2, imgPts);
+                    Cv2.ImShow("img", img);
+                    if (Cv2.WaitKey(0) == 27)
+                        break;
+                }
+            }
+        }
 
-//                    // project 3D points to image plane
-//                    Cv2.ProjectPoints(axis, rvecs2, tvecs2, mtx, dist, imgpts, jac);
+        private static IEnumerable<Point3f> Create3DChessboardCorners(Size boardSize, float squareSize)
+        {
+            for (int y = 0; y < boardSize.Height; y++)
+            {
+                for (int x = 0; x < boardSize.Width; x++)
+                {
+                    yield return new Point3f(x * squareSize, y * squareSize, 0);
+                }
+            }
+        }
 
-//                    Cv2.ImShow("imgpts", imgpts);
-
-//                    //img = draw(img, corners2, imgpts);
-//                    Cv2.ImShow("img", img);
-//                    if (Cv2.WaitKey(0) == 27)
-//                        break;
-//                }
-//            }
-//        }
-
-//        static List<Point3f> GetChessboardObjectPoints(Size boardSize, float squareSize)
-//        {
-//            List<Point3f> objectPoints = new List<Point3f>() { };
-//            for (int i = 0; i < boardSize.Height; ++i)
-//            {
-//                for (int j = 0; j < boardSize.Width; ++j)
-//                {
-//                    objectPoints.Add(new Point3f(squareSize * j, squareSize * i, 0.0f));
-//                }
-//            }
-//            return objectPoints;
-//        }
-
-//        public static Mat[] calcBoardCornerPositions(Size BoardSize, float SquareSize, int imagesCount)
-//        {
-//            Mat[] corners = new Mat[imagesCount];
-//            for (int k = 0; k < imagesCount; k++)
-//            {
-//                Point3f[] p = new Point3f[BoardSize.Height * BoardSize.Width];
-//                for (int i = 0; i < BoardSize.Height; i++)
-//                {
-//                    for (int j = 0; j < BoardSize.Width; j++)
-//                    {
-//                        p[i * BoardSize.Width + j] = new Point3f(j * SquareSize, i * SquareSize, 0);
-//                    }
-//                }
-//                corners[k] = Mat.FromArray<Point3f>(p);
-//            }
-//            return corners;
-//        }
-
-//        static Mat draw(Mat img, Point2f[] corners, Point2f[] imgpts)
-//        {
-//            Point l0 = new Point((int)corners[0].X, (int)corners[0].Y);
-//            Point l1 = new Point((int)imgpts[0].X, (int)imgpts[0].Y);
-//            Point l2 = new Point((int)imgpts[1].X, (int)imgpts[1].Y);
-//            Point l3 = new Point((int)imgpts[2].X, (int)imgpts[2].Y);
-//            Cv2.Line(img, l0, l1, new Scalar(255, 0, 0), 5);
-//            Cv2.Line(img, l0, l2, new Scalar(0, 255, 0), 5);
-//            Cv2.Line(img, l0, l3, new Scalar(0, 0, 255), 5);
-//            return img;
-//        }
-//    }
-//}
+        static Mat draw(Mat img, Point2f[] corners, Point2f[] imgpts)
+        {
+            Point l0 = new Point((int)corners[0].X, (int)corners[0].Y);
+            Point l1 = new Point((int)imgpts[0].X, (int)imgpts[0].Y);
+            Point l2 = new Point((int)imgpts[1].X, (int)imgpts[1].Y);
+            Point l3 = new Point((int)imgpts[2].X, (int)imgpts[2].Y);
+            Cv2.Line(img, l0, l1, new Scalar(255, 0, 0), 5);
+            Cv2.Line(img, l0, l2, new Scalar(0, 255, 0), 5);
+            Cv2.Line(img, l0, l3, new Scalar(0, 0, 255), 5);
+            return img;
+        }
+    }
+}
--- C++/姿势估计/OpenCvDemo.cpp
+++ C++/姿势估计/OpenCvDemo.cpp
@@ -6,179 +6,140 @@ OpenCv版本 opencv-4.5.5-vc14_vc15
 时间：2023.11
 */
 
-//【没搞定。。。】
+#include <opencv2/opencv.hpp>
+#include <opencv2/ml.hpp>
+#include <opencv2/cvconfig.h>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>    
 
-//#include <opencv2/opencv.hpp>
-//#include <opencv2/ml.hpp>
-//#include <opencv2/cvconfig.h>
-//#include <opencv2/imgproc/imgproc.hpp>
-//#include <opencv2/highgui/highgui.hpp>    
-//
-//#include <io.h>
-//#include <string>
-//#include <iostream>
-//#include <ctime>
-//
-//using namespace cv;
-//using namespace std;
-//
-//std::vector<cv::Point3f> GetChessboardObjectPoints(const cv::Size& boardSize, float squareSize) {
-//    std::vector<cv::Point3f> objectPoints;
-//    for (int i = 0; i < boardSize.height; ++i) {
-//        for (int j = 0; j < boardSize.width; ++j) {
-//            objectPoints.push_back(Point3f(squareSize * j, squareSize * i, 0.0f));
-//        }
-//    }
-//    return objectPoints;
-//}
-//
-//void demo1()
-//{
-//    // 获取图片目录
-//    std::string inPath1 = "..\\images\\lefts\\left*.jpg";
-//    std::string inPath2 = "..\\images\\rights\\right*.jpg";
-//    intptr_t handle1, handle2;
-//    struct _finddata_t fileinfo1, fileinfo2;
-//    handle1 = _findfirst(inPath1.c_str(), &fileinfo1);
-//    handle2 = _findfirst(inPath2.c_str(), &fileinfo2);
-//
-//    vector<string> images;
-//    do {
-//        string Name = fileinfo1.name;
-//        images.push_back("..\\images\\lefts\\" + Name);
-//    } while (!_findnext(handle1, &fileinfo1));
-//
-//    do {
-//        string Name = fileinfo2.name;
-//        images.push_back("..\\images\\lefts\\" + Name);
-//    } while (!_findnext(handle2, &fileinfo2));
-//
-//    TermCriteria criteria = TermCriteria(TermCriteria::EPS | TermCriteria::MAX_ITER, 30, 0.001);
-//
-//    // 棋盘格内角点数量（行数和列数）
-//    cv::Size chessboardSize(9, 6);
-//
-//    // 棋盘格尺寸（单位：米）
-//    float squareSize = 0.025f;
-//
-//    // 存储棋盘格角点坐标的容器
-//    std::vector<std::vector<cv::Point3f>> objpoints;
-//    // 存储检测到的棋盘格角点坐标的容器
-//    std::vector<std::vector<cv::Point2f>> imgpoints;
-//    Mat gray;
-//
-//    for (int i = 0; i < images.size(); i++)
-//    {
-//        Mat img = imread(images[i]);
-//        if (!img.empty())
-//        {
-//            cvtColor(img, gray, COLOR_BGR2GRAY);
-//
-//            // Find the chess board corners
-//            std::vector<cv::Point2f> corners;
-//            bool ret = findChessboardCorners(gray, chessboardSize, corners);
-//
-//            // If found, add object points, image points(after refining them)
-//            if (ret == true)
-//            {
-//                cornerSubPix(gray, corners, Size(11, 11), Size(-1, -1), criteria);
-//                // Draw and display the corners
-//                drawChessboardCorners(img, chessboardSize, corners, ret);
-//
-//                objpoints.push_back(GetChessboardObjectPoints(chessboardSize, squareSize));
-//                imgpoints.push_back(corners);
-//
-//                imshow("img", img);
-//                waitKey(10);
-//            }
-//        }
-//    }
-//
-//    // 执行相机标定
-//    cv::Mat mtx, dist;
-//    std::vector<cv::Mat> rvecs, tvecs;
-//    cv::calibrateCamera(objpoints, imgpoints, cv::Size(gray.cols, gray.rows), mtx, dist, rvecs, tvecs);
-//
-//
-//
-//    for (int i = 0; i < images.size(); i++)
-//    {
-//        Mat img = imread(images[i]);
-//        cvtColor(img, gray, COLOR_BGR2GRAY);
-//        // Find the chess board corners
-//        std::vector<cv::Point2f> corners;
-//        bool ret = findChessboardCorners(gray, chessboardSize, corners);
-//        if (ret == true)
-//        {
-//            cornerSubPix(gray, corners, Size(11, 11), Size(-1, -1), criteria);
-//
-//            cv::Mat rvec;  // 输出的旋转向量
-//            cv::Mat tvec;  // 输出的平移向量
-//            solvePnP(objpoints[0], imgpoints[0], mtx, dist, rvec, tvec);
-//       
-//
-//            // Find the rotation and translation vectors.
-//            solvePnP(objq, Mat.FromArray<Point2f>(corners2), mtx, dist, rvecs2, tvecs2);
-//
-//            // project 3D points to image plane
-//            Cv2.ProjectPoints(axis, rvecs2, tvecs2, mtx, dist, imgpts, jac);
-//
-//            //img = draw(img, corners2, imgpts);
-//            Cv2.ImShow("img", img);
-//            if (Cv2.WaitKey(0) == 27)
-//                break;
-//        }
-//    }
-//
-//
-//
-//
-//
-//
-//
-//
-//   // // 畸变校正
-//   // Mat img = imread("../images/lefts/left12.jpg");
-//   // int h = img.rows;
-//   // int w = img.cols;
-//   // cv::Mat mapx, mapy;
-//   // Rect roi;
-//   // cv::Mat newcameramtx = getOptimalNewCameraMatrix(mtx, dist, Size(w,h), 1, Size(w, h), &roi);
-//
-//   // // undistort
-//   // Mat dst;
-//   // undistort(img, dst, mtx, dist, newcameramtx);
-//
-//   // // crop the image
-//   // dst = dst(roi);
-//   // imshow("calibresult1.png", dst);
-//
-//   // initUndistortRectifyMap(mtx, dist, cv::Mat(), newcameramtx, Size(w, h), 5, mapx, mapy);
-//   // remap(img, dst, mapx, mapy, INTER_LINEAR);
-//
-//   // // crop the image
-//   // dst = dst(roi);
-//   // imshow("calibresult2.png", dst);
-//   //// 你会发现结果图像中所有的边界变直了
-//
-//   // // 反向投影误差
-//   // // 我们可以利用反向投影误差对我们找到的参数的准确性进行估计。得到的结果越接近 0越好。
-//   // // 有了内部参数，畸变参数和旋转变换矩阵，我们就可以使用cv2.projectPoints()将对象点转换到图像点。
-//   // // 然后就可以计算变换得到图像与角点检测算法的绝对差了。
-//   // // 然后我们计算所有标定图像的误差平均值。
-//   // int mean_error = 0;
-//   // for (int i = 0; i < objpoints.size(); i++)
-//   // {
-//   //     vector<Point2f> imgpoints2;
-//   //     projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist, imgpoints2);
-//   //    int error = norm(imgpoints[i], imgpoints2, NORM_L2) / imgpoints2.size();
-//   //    mean_error += error;
-//   //    cout << "total error :" << mean_error / objpoints.size() << endl;
-//   // }
-//   // waitKey(0);
-//}
-//
-//int main()
-//{
-//    demo1();
-//}
+#include <io.h>
+#include <string>
+#include <iostream>
+#include <ctime>
+
+using namespace cv;
+using namespace std;
+
+vector<Point3f> Create3DChessboardCorners(Size boardSize, float squareSize)
+{
+    std::vector<cv::Point3f> objectPoints;
+    for (int y = 0; y < boardSize.height; y++)
+    {
+        for (int x = 0; x < boardSize.width; x++)
+        {
+            objectPoints.push_back(Point3f(x * squareSize, y * squareSize, 0));
+        }
+    }
+    return objectPoints;
+}
+
+Mat draw(Mat img, vector<Point2f> corners, vector<Point2f> imgpts)
+{
+    Point l0((int)corners[0].x, (int)corners[0].y);
+    Point l1((int)imgpts[0].x, (int)imgpts[0].y);
+    Point l2((int)imgpts[1].x, (int)imgpts[1].y);
+    Point l3((int)imgpts[2].x, (int)imgpts[2].y);
+    line(img, l0, l1, Scalar(255, 0, 0), 5);
+    line(img, l0, l2, Scalar(0, 255, 0), 5);
+    line(img, l0, l3, Scalar(0, 0, 255), 5);
+    return img;
+}
+
+int main()
+{
+    // 获取图片目录
+    std::string inPath1 = "..\\images\\lefts\\left*.jpg";
+    std::string inPath2 = "..\\images\\rights\\right*.jpg";
+    intptr_t handle1, handle2;
+    struct _finddata_t fileinfo1, fileinfo2;
+    handle1 = _findfirst(inPath1.c_str(), &fileinfo1);
+    handle2 = _findfirst(inPath2.c_str(), &fileinfo2);
+
+    vector<string> images;
+    do {
+        string Name = fileinfo1.name;
+        images.push_back("..\\images\\lefts\\" + Name);
+    } while (!_findnext(handle1, &fileinfo1));
+
+    do {
+        string Name = fileinfo2.name;
+        images.push_back("..\\images\\lefts\\" + Name);
+    } while (!_findnext(handle2, &fileinfo2));
+
+    TermCriteria criteria = TermCriteria(TermCriteria::EPS | TermCriteria::MAX_ITER, 30, 0.001);
+
+    // 棋盘格内角点数量（行数和列数）
+    cv::Size BoardSize(9, 6);
+
+    // 棋盘格尺寸（单位：米）
+    float squareSize = 0.025f;
+
+    // 存储棋盘格角点坐标的容器
+    std::vector<std::vector<cv::Point3f>> objpoints;
+    // 存储检测到的棋盘格角点坐标的容器
+    std::vector<std::vector<cv::Point2f>> imgpoints;
+    Mat gray;
+
+    for (int i = 0; i < images.size(); i++)
+    {
+        Mat img = imread(images[i]);
+        if (!img.empty())
+        {
+            cvtColor(img, gray, COLOR_BGR2GRAY);
+
+            // Find the chess board corners
+            std::vector<cv::Point2f> corners;;
+            bool found = findChessboardCorners(gray, BoardSize, corners);
+            if (found == true)
+            {
+                vector<Point3f> objectPointsArray = Create3DChessboardCorners(BoardSize, squareSize);
+                objpoints.push_back(objectPointsArray);
+                imgpoints.push_back(corners);
+            }
+        }
+    }
+
+    cv::Mat mtx, dist;
+    std::vector<cv::Mat> rvecs, tvecs;
+    cv::calibrateCamera(objpoints, imgpoints, gray.size(), mtx, dist, rvecs, tvecs);
+
+    vector<Point3f> objPts;
+    for (int i = 0; i < 6; i++)
+    {
+        for (int j = 0; j < 9; j++)
+        {
+            objPts.push_back(Point3f((float)j, (float)i, (float)0));
+        }
+    }
+    vector<Point3f> axis;
+    axis.push_back(Point3f(3, 0, 0));
+    axis.push_back(Point3f(0, 3, 0));
+    axis.push_back(Point3f(0, 0, -3));
+
+    for (int i = 0; i < images.size(); i++)
+    {
+        Mat img = imread(images[i]);
+        cvtColor(img, gray, COLOR_BGR2GRAY);
+
+        std::vector<cv::Point2f> corners;
+        bool found = findChessboardCorners(gray, BoardSize, corners);
+        if (found == true)
+        {
+            cornerSubPix(gray, corners, Size(11, 11), Size(-1, -1), criteria);
+
+            cv::Mat rvec;  // 输出的旋转向量
+            cv::Mat tvec;  // 输出的平移向量
+            solvePnP(objPts, corners, mtx, dist, rvec, tvec);
+
+            // project 3D points to image plane
+            cv::Mat imgpts;
+            cv::Mat jac;
+            projectPoints(axis, rvec, tvec, mtx, dist, imgpts, jac);
+
+            img = draw(img, corners, imgpts);
+            imshow("img", img);
+            if (waitKey(0) == 27)
+                break;
+        }
+    }
+}
